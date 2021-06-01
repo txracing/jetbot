e996a94bd7f9a1a62c93e21df9e457a095af2272
txracing <txracing.d@gmail.com>
2021-06-01 17:39:07 +0900

    Update configure.sh

--- docker/configure.sh
+++ docker/configure.sh
@@ -12,9 +12,12 @@ export L4T_VERSION="$L4T_RELEASE.$L4T_REVISION"
 if [[ $L4T_VERSION = "32.4.3" ]]
 then
 	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.3-pth1.6-py3
-elif [[ "$L4T_VERSION" == "32.4.4" ]]
+if [[ "$L4T_VERSION" == "32.4.4" ]]
 then
 	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.4.4-pth1.6-py3
+elif [[ "$L4T_VERSION" == "32.5.1" ]]
+then
+	JETBOT_BASE_IMAGE=nvcr.io/nvidia/l4t-pytorch:r32.5.0-pth1.6-py3
 else
 	echo "JETBOT_BASE_IMAGE not found for ${L4T_VERSION}.  Please manually set the JETBOT_BASE_IMAGE environment variable. (ie: export JETBOT_BASE_IMAGE=...)"
 fi
